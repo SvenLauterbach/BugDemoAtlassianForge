020b0ffc3624b2e3c51bd0a38bcc46fe7b26f3fb
Sven Lauterbach <svenlauterbach@live.de>
2021-09-12 20:47:03 +0200

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,66 @@
+import ForgeUI, { render, ProjectPage, Fragment, Text, Button, useProductContext, useState } from '@forge/ui';
+import api, { route } from '@forge/api';
+
+const App = () => {
+
+    const [response, setResponse] = useState("Click the Button");
+    const context = useProductContext();
+    const createIssue = async () => {
+        var bodyData = {
+            "fields": {
+              "summary": "Test",
+              "project": {
+                "key": context.platformContext.projectKey
+              },
+              "issuetype": {
+                "name": "Story"
+              },
+              "description": {
+                "type": "doc",
+                "version": 1,
+                "content": [
+                  {
+                    "type": "paragraph",
+                    "content": [
+                      {
+                        "text": "Test description",
+                        "type": "text"
+                      }
+                    ]
+                  }
+                ]
+              },
+              "labels": [
+                "testlabel"
+              ]
+            }
+          };
+                
+          const response = await api.asApp().requestJira(route`/rest/api/3/issue`, {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(bodyData)
+          });
+        
+        
+          const json = await response.json();
+
+            setResponse(JSON.stringify(json));
+      };
+
+    return (
+        <Fragment>
+            <Text>{response}</Text>
+            <Button text="Create Issue" onClick={createIssue} />
+        </Fragment>
+    );
+};
+
+export const run = render(
+    <ProjectPage>
+        <App />
+    </ProjectPage>
+);
