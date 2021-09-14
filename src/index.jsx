df8f544eaa8f8adda3c9518fb05df20d79dff1a3
Sven Lauterbach <svenlauterbach@live.de>
2021-09-14 21:22:56 +0200

    Use isseType.id

--- src/index.jsx
+++ src/index.jsx
@@ -3,64 +3,78 @@ import api, { route } from '@forge/api';
 
 const App = () => {
 
-    const [response, setResponse] = useState("Click the Button");
-    const context = useProductContext();
-    const createIssue = async () => {
-        var bodyData = {
-            "fields": {
-              "summary": "Test",
-              "project": {
-                "key": context.platformContext.projectKey
-              },
-              "issuetype": {
-                "name": "Story"
-              },
-              "description": {
-                "type": "doc",
-                "version": 1,
-                "content": [
-                  {
-                    "type": "paragraph",
-                    "content": [
-                      {
-                        "text": "Test description",
-                        "type": "text"
-                      }
-                    ]
-                  }
-                ]
-              },
-              "labels": [
-                "testlabel"
+  const [response, setResponse] = useState("Click the Button");
+  const context = useProductContext();
+
+  const createIssue = async () => {
+
+    const projectKey = context.platformContext.projectKey;
+
+    const responseGetProject = await api.asApp().requestJira(route`/rest/api/3/project/${projectKey}?expand=issueTypes`, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    const jsonGetProject = await responseGetProject.json();
+
+    const issueType = jsonGetProject.issueTypes.find(issueType => issueType.name == "Story");
+
+    var bodyData = {
+      "fields": {
+        "summary": "Test",
+        "project": {
+          "key": projectKey
+        },
+        "issuetype": {
+          "id": issueType.id
+        },
+        "description": {
+          "type": "doc",
+          "version": 1,
+          "content": [
+            {
+              "type": "paragraph",
+              "content": [
+                {
+                  "text": "Test description",
+                  "type": "text"
+                }
               ]
             }
-          };
-                
-          const response = await api.asApp().requestJira(route`/rest/api/3/issue`, {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(bodyData)
-          });
-        
-        
-          const json = await response.json();
+          ]
+        },
+        "labels": [
+          "testlabel"
+        ]
+      }
+    };
 
-            setResponse(JSON.stringify(json));
-      };
+    const response = await api.asApp().requestJira(route`/rest/api/3/issue`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(bodyData)
+    });
 
-    return (
-        <Fragment>
-            <Text>{response}</Text>
-            <Button text="Create Issue" onClick={createIssue} />
-        </Fragment>
-    );
+
+    const json = await response.json();
+
+    setResponse(JSON.stringify(json));
+  };
+
+  return (
+    <Fragment>
+      <Text>{response}</Text>
+      <Button text="Create Issue" onClick={createIssue} />
+    </Fragment>
+  );
 };
 
 export const run = render(
-    <ProjectPage>
-        <App />
-    </ProjectPage>
+  <ProjectPage>
+    <App />
+  </ProjectPage>
 );
